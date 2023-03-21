570c57fc426f0722352245281ef43081b9ae4828
andu0610 <127616539+andu0610@users.noreply.github.com>
2023-03-21 20:08:17 +0200

    Add files via upload

---
+++ tipuri de date.py
@@ -0,0 +1,70 @@
+###TUPLE###
+
+# tuplul_meu=('1',2,'ardei')
+# print (type(tuplul_meu))
+# print(tuplul_meu)
+#la tuple nu poti edita ekementele
+#nu poti adauga elemente
+#nu poti sterge elemente
+# print(tuplul_meu[1])
+
+# tuplu2=(5)
+# print(type(tuplu2))
+# tuplu3=(5, )
+# print(type(tuplu3))
+
+# tuplu=('unu','doi','trei','patru')
+# print(tuplu[2])
+# print(tuplu[-1])
+
+# (var1, var2, var3, var4)=tuplu
+# print(var1)
+# print(var2)
+
+# tuplu=('unu','doi','trei','patru')
+# tuplu2=(1,2,3)
+# print(tuplu+tuplu2)
+# tuplu_r=(tuplu+tuplu2)*2
+# print(tuplu_r)
+
+# tuplu=('unu','doi','trei','patru')
+# # print(tuplu[2:])
+# # print(tuplu[-2:])
+# # print(tuplu[1:3])
+# print('trei' in tuplu)
+
+# print(tuplu.count('2'))
+# print(tuplu.index('trei'))
+# print(len(tuplu))
+# print(max(tuplu))
+# print(min(tuplu))
+
+
+# tuplu=('unu','doi','trei','patru')
+# tuplu_r=tuple(sorted(tuplu))
+# print(tuplu)
+# print(tuplu_r)
+# x=('unu','doi','trei','patru')
+# y=list(x)
+# y[1]='kiwi'
+# x=tuple(y)
+# print(x)
+# x=('unu','doi','trei','patru')
+# y=list(x)
+# y.append('orange')
+# x=tuple(y)
+# print(x)
+
+###DICTIONARY###
+
+# dictionar={'nume': 'gigel', 
+#            'prenume':'craciun',
+#            'varsta': 30}
+# print(dictionar)
+# print(dictionar['varsta'])
+# dictionar['nume']='mos'
+# print(dictionar)
+# print(type(dictionar))
+
+# un_set={'unu','doi','trei', True,1,2}
+# print(un_set)
